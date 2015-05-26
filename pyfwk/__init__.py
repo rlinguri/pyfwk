b9c8f8024961c49bf56a52c22027785bf3604240
Linguri Technology <rlinguri@mac.com>
2015-05-26 16:05:24 -0600

    Pass through of class names to package level

--- pyfwk/__init__.py
+++ pyfwk/__init__.py
@@ -2,3 +2,9 @@
 
 __package__ = 'pyfwk'
 __author__ = 'Roderic Linguri'
+
+from base import *
+from struc import *
+from utils import *
+
+__all__ = ['DBase','Model','Object','DBCol','DBRow','FileManager','Increment']
