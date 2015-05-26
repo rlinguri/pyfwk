f560d65b42c3fade62b57496677beb4ddf5ff4ed
Linguri Technology <rlinguri@mac.com>
2015-05-26 11:13:15 -0600

    Update __init__ to export DBCol and DBRow class

--- pyfwk/struc/__init__.py
+++ pyfwk/struc/__init__.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
 
-__module__ = 'struc'
+
+__module__ = 'pyfwk.struc'
 __author__ = 'Roderic Linguri'
+
+from dbcol import *
+from dbrow import *
+
+__all__ = ['DBCol', 'DBRow']
